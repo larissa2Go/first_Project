f8f3a6760ae44381c702967152d59abfec7a0896
Larissa Sowada <larissasowada@Mac.fritz.box>
2026-04-23 14:17:39 +0200

    layout structure created

--- src/main.js
+++ src/main.js
@@ -6,61 +6,26 @@ import { setupCounter } from './counter.js'
 import './style.css'
 
 document.querySelector('#app').innerHTML = `
-<section id="center">
-  <div class="hero">
-    <img src="${heroImg}" class="base" width="170" height="179">
-    <img src="${javascriptLogo}" class="framework" alt="JavaScript logo"/>
-    <img src="${viteLogo}" class="vite" alt="Vite logo" />
-  </div>
-  <div>
-    <h1>Get started</h1>
-    <p>Edit <code>src/main.js</code> and save to test <code>HMR</code></p>
-  </div>
-  <button id="counter" type="button" class="counter"></button>
-</section>
+<header class="bg-white shadow-md sticky top-0 z-50">
+    <h1>Menu Bar</h1>
+</header>
 
-<div class="ticks"></div>
 
-<section id="next-steps">
-  <div id="docs">
-    <svg class="icon" role="presentation" aria-hidden="true"><use href="/icons.svg#documentation-icon"></use></svg>
-    <h2>Documentation</h2>
-    <p>Your questions, answered</p>
-    <ul>
-      <li>
-        <a href="https://vite.dev/" target="_blank">
-          <img class="logo" src="${viteLogo}" alt="" />
-          Explore Vite
-        </a>
-      </li>
-      <li>
-        <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-          <img class="button-icon" src="${javascriptLogo}" alt="">
-          Learn more
-        </a>
-      </li>
-    </ul>
-  </div>
-  <div id="social">
-    <svg class="icon" role="presentation" aria-hidden="true"><use href="/icons.svg#social-icon"></use></svg>
-    <h2>Connect with us</h2>
-    <p>Join the Vite community</p>
-    <ul>
-      <li><a href="https://github.com/vitejs/vite" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#github-icon"></use></svg>GitHub</a></li>
-      <li><a href="https://chat.vite.dev/" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#discord-icon"></use></svg>Discord</a></li>
-      <li><a href="https://x.com/vite_js" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#x-icon"></use></svg>X.com</a></li>
-      <li><a href="https://bsky.app/profile/vite.dev" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#bluesky-icon"></use></svg>Bluesky</a></li>
-    </ul>
-    <h1><bold>HI hier ist larissa</bold></h1>
-  </div>
-</section>
+<aside class="sidebar">
+    <nav aria-label="Hauptnavigation">
+        <ul>
+            <li><a href="#">Ausbilder</a></li>
+            <li><a href="#">Auzubis</a></li>
+            <li><a href="#">Berichtsheft</a></li>
+            <li><a href="#">Einstellungen</a></li>
+        </ul>
+    </nav>
+</aside>
 
-<div class="ticks"></div>
-<section id="spacer"></sectçion>
+<main>
+<articel>Auf dieser Seite könnt ihr vieles über eure Aubildung lernen. Klickt euch doch gerne durch.</articel>
+</main>
 
-<h1 class="text-red-500 text-3xl">
-  Hallo Tailwind
-</h1>
 `
 
 setupCounter(document.querySelector('#counter'))
