e4a191f880f85b74f54b6d79ab0685422d29fd7c
Larissa Sowada <larissasowada@Mac.fritz.box>
2026-04-23 14:39:43 +0200

    layout structure change in index.html

--- src/main.js
+++ src/main.js
@@ -6,25 +6,7 @@ import { setupCounter } from './counter.js'
 import './style.css'
 
 document.querySelector('#app').innerHTML = `
-<header class="bg-white shadow-md sticky top-0 z-50">
-    <h1>Menu Bar</h1>
-</header>
 
-
-<aside class="sidebar">
-    <nav aria-label="Hauptnavigation">
-        <ul>
-            <li><a href="#">Ausbilder</a></li>
-            <li><a href="#">Auzubis</a></li>
-            <li><a href="#">Berichtsheft</a></li>
-            <li><a href="#">Einstellungen</a></li>
-        </ul>
-    </nav>
-</aside>
-
-<main>
-<articel>Auf dieser Seite könnt ihr vieles über eure Aubildung lernen. Klickt euch doch gerne durch.</articel>
-</main>
 
 `
 
