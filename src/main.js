6a3ce11c957b375cb7bcc1c400aa8f838a931e63
Larissa Sowada <larissasowada@MacBook-Pro-von-Larissa.fritz.box>
2026-04-28 16:29:51 +0200

    layout loads

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
+import './style.css'
 import javascriptLogo from './assets/javascript.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import { setupCounter } from './counter.js'
-import './style.css'
+
 
 const btn = document.querySelector('#theme-toggle')
 const html = document.documentElement
