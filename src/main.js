b9421ce12f0278449fbc38b21e278f748a6719dc
Larissa Sowada <larissasowada@MBP-von-Larissa.fritz.box>
2026-04-29 14:35:23 +0200

    add skeleton into image

--- src/main.js
+++ src/main.js
@@ -24,4 +24,30 @@ button.addEventListener("click", () => {
   sidebar.classList.toggle("hidden")
 })
 
+const highContrastButton = document.querySelector("#high-contrast-toggle")
+
+highContrastButton.addEventListener("click", () => {
+    if (html.dataset.theme === 'light') {
+      html.dataset.theme = 'high-contrast'
+    } else {
+      html.dataset.theme = 'light'
+    }
+  })
+
+const skeleton = document.querySelector('#skeleton')
+const image = document.querySelector('#learn-image')
+
+
+if (image.complete) {
+  skeleton.classList.add('hidden')
+  image.classList.remove('opacity-0')
+  image.classList.add('opacity-100')
+} else {
+  image.addEventListener('load', () => {
+    skeleton.classList.add('hidden')
+    image.classList.remove('opacity-0')
+    image.classList.add('opacity-100')
+  })
+}
+
 
